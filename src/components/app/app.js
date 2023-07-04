565a46587df084fc3ed0e0a31d45d58050d0b156
OlegPichkur <oleg1pichkur@gmail.com>
2023-07-04 15:46:47 +0300

    доп.функционал п,133 Udemy

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,6 +7,13 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form.js';
 import './app.css';
 
 function App() {
+
+   const data = [
+      { name: "Anton Petrov", salary: 1400, increase: true, id: 1 },
+      { name: "Ted Ivanov", salary: 1700, increase: false, id: 2 },
+      { name: "Julia Roberts", salary: 900, increase: true, id: 3 },
+   ];
+
    return (
       <div className="app">
          <AppInfo />
@@ -16,7 +23,7 @@ function App() {
             <AppFilter/>
          </div>
 
-         <EmployeesList />
+         <EmployeesList data={data} />
          <EmployeesAddForm/>
       </div>
    );
